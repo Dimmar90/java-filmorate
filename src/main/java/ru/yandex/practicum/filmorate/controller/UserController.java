c3227ad6e61862098c9647711083433c37452bb0
Dimmar90 <106697499+Dimmar90@users.noreply.github.com>
2023-03-30 03:07:40 +0300

    UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.apache.catalina.valves.JsonErrorReportValve;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,8 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
-import javax.validation.*;
-import java.util.*;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
