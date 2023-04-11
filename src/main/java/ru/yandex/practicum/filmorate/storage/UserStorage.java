860c4602182ea69ba7eb90fa9f4f4a3c95997b68
Dimmar90 <106697499+Dimmar90@users.noreply.github.com>
2023-04-12 01:26:40 +0300

    add friends likes

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -8,11 +8,18 @@ import java.util.Set;
 
 public interface UserStorage {
     void addUser(User user);
+
     void updateUser(User user);
+
     User getUserById(long id);
+
     Map<Long, User> getAllUsers();
+
     void addFriend(User user, User friend);
+
     Set<User> getUserFriendList(User user);
+
     List<User> getCommonListOfFriends(User user, User usersFriend);
+
     void deleteFriend(User user, User friend);
 }
